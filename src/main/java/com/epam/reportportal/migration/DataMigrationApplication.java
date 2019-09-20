b87e027530c27bcd9bcdd9abc04100c6fe6168be
pavel_bortnik <pavel_bortnik@epam.com>
2019-09-20 19:20:03 +0300

    clean up

--- src/main/java/com/epam/reportportal/migration/DataMigrationApplication.java
+++ src/main/java/com/epam/reportportal/migration/DataMigrationApplication.java
@@ -1,10 +1,43 @@
 package com.epam.reportportal.migration;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class DataMigrationApplication {
+
+	@Bean
+	public Cache<String, Long> customStatisticsFieldsCache() {
+		return Caffeine.newBuilder().initialCapacity(50).maximumSize(50).expireAfterAccess(10, TimeUnit.MINUTES).build();
+	}
+
+	@Bean(name = "statisticsFields")
+	// Default statistics fields have fixed ids in PostgreSQL
+	public Map<String, Long> statisticsFields() {
+		Map<String, Long> statisticsFields = new HashMap<>(14);
+		statisticsFields.put("total", 1L);
+		statisticsFields.put("passed", 2L);
+		statisticsFields.put("skipped", 3L);
+		statisticsFields.put("failed", 4L);
+		statisticsFields.put("statistics$defects$automation_bug$total", 5L);
+		statisticsFields.put("statistics$defects$automation_bug$ab001", 6L);
+		statisticsFields.put("statistics$defects$product_bug$total", 7L);
+		statisticsFields.put("statistics$defects$product_bug$pb001", 8L);
+		statisticsFields.put("statistics$defects$system_issue$total", 9L);
+		statisticsFields.put("statistics$defects$system_issue$si001", 10L);
+		statisticsFields.put("statistics$defects$to_investigate$total", 11L);
+		statisticsFields.put("statistics$defects$to_investigate$ti001", 12L);
+		statisticsFields.put("statistics$defects$no_defect$total", 13L);
+		statisticsFields.put("statistics$defects$no_defect$nd001", 14L);
+		return statisticsFields;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataMigrationApplication.class, args);
